85759d15a1a411c598f9f00cff29ab4438d88d96
Ranji <ranjinie.souria@gmail.com>
2021-12-13 11:08:26 +0100

    Creating an interface to write the data

--- Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/ISymptomWriter.java
@@ -1,7 +1,5 @@
 package com.hemebiotech.analytics;
 
-import java.util.List;
-
 /**
  * 
  * Interface to write the data
@@ -9,9 +7,7 @@ import java.util.List;
  */
 public interface ISymptomWriter {
 	/**
-	 * If no data is available, return an empty List
-	 * 
-	 * @return a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
+	 * Writes the results
 	 */
-	List<String> writeSymptoms ();
+	void writeSymptoms();
 }
